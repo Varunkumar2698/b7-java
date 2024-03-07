b36cb62a3ac4944f784ce3e6562e02eaa06b4b1f
Varunkumar2698 <127874669+Varunkumar2698@users.noreply.github.com>
2024-03-07 14:47:52 +0530

    Update AppTest.java

--- src/test/java/com/mycompany/app/AppTest.java
+++ src/test/java/com/mycompany/app/AppTest.java
@@ -12,37 +12,37 @@ import static org.junit.Assert.*;
  */
 public class AppTest
 {
-
-    private final ByteArrayOutputStream outContent = new ByteArrayOutputStream();
-
-    @Before
-    public void setUpStreams() {
-        System.setOut(new PrintStream(outContent));
-    }
-
-    @Test
-    public void testAppConstructor() {
-        try {
-            new App();
-        } catch (Exception e) {
-            fail("Construction failed.");
+    public static void main(String[] args) {
+        private final ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+    
+        @Before
+        public void setUpStreams() {
+            System.setOut(new PrintStream(outContent));
+        }
+    
+        @Test
+        public void testAppConstructor() {
+            try {
+                new App();
+            } catch (Exception e) {
+                fail("Construction failed.");
+            }
+        }
+    
+        @Test
+        public void testAppMain()
+        {
+            App.main(null);
+            try {
+                assertEquals("Hello World!!" + System.getProperty("line.separator"), outContent.toString());
+            } catch (AssertionError e) {
+                fail("\"message\" is not \"Hello World!!\"");
+            }
+        }
+    
+        @After
+        public void cleanUpStreams() {
+            System.setOut(null);
         }
     }
-
-    @Test
-    public void testAppMain()
-    {
-        App.main(null);
-        try {
-            assertEquals("Hello World!!" + System.getProperty("line.separator"), outContent.toString());
-        } catch (AssertionError e) {
-            fail("\"message\" is not \"Hello World!!\"");
-        }
-    }
-
-    @After
-    public void cleanUpStreams() {
-        System.setOut(null);
-    }
-
 }
